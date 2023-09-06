d5e17f0e22adbbc68f32a5dfdfc5d32cef6c643d
mikecoughlan <mike.k.coughlan@gmail.com>
2023-09-06 17:57:23 -0400

    debugging

--- field_line_tracing/placing_field_line_footpoints_on_twins_maps.py
+++ field_line_tracing/placing_field_line_footpoints_on_twins_maps.py
@@ -30,6 +30,13 @@ region_numbers = [83, 143, 223, 44, 173, 321, 366, 383, 122, 279, 14, 95, 237, 2
 
 
 def loading_dicts():
+	'''
+	Loads the regional dictionaries and stats dictionaries
+
+	Returns:
+		regions (dict): dictionary containing the regional dictionaries
+		stats (dict): dictionary containing the regional stats dictionaries including rsd and mlt data
+	'''
 
 	print('Loading regional dictionaries....')
 
@@ -72,7 +79,7 @@ def loading_solarwind():
 
 	print('Loading solar wind data....')
 	df = pd.read_feather('../data/SW/ace_data.feather')
-	df.set_index('Date_UTC', inplace=True, drop=True)
+	df.set_index('ACEepoch', inplace=True, drop=True)
 	df.index = pd.to_datetime(df.index, format='%Y-%m-%d %H:%M:$S')
 
 	return df
@@ -103,21 +110,20 @@ def combining_regional_dfs(stations, rsd, map_keys, delay=None):
 		stat = loading_supermag(station)
 		stat = stat[start_time:end_time]
 		stat = stat[['dbht']]
-		for col in stat.columns:
-			if delay:
-				stat[f'{col}_delay_{delay}'] = stat[col].shift(-delay)
-				combined_stations = pd.concat([combined_stations, stat[f'{col}_delay_{delay}']], axis=1, ignore_index=False)
-			else:
-				stat[f'{col}_dbdt'] = stat[col]
-				combined_stations = pd.concat([combined_stations, stat[f'{col}_dbdt']], axis=1, ignore_index=False)
+		if delay:
+			stat[f'{station}_delay_{delay}'] = stat['dbht'].shift(-delay)
+			combined_stations = pd.concat([combined_stations, stat[f'delay_{delay}']], axis=1, ignore_index=False)
+		else:
+			stat[f'{station}_dbdt'] = stat['dbht']
+			combined_stations = pd.concat([combined_stations, stat[f'{station}_dbdt']], axis=1, ignore_index=False)
 
 	mean_dbht = combined_stations.mean(axis=1)
 	max_dbht = combined_stations.max(axis=1)
 
 	combined_stations['reg_mean'] = mean_dbht
 	combined_stations['reg_max'] = max_dbht
-	combined_stations['rsd'] = rsd['max_rsd']
-	combined_stations['MLT'] = rsd['MLT']
+	combined_stations['rsd'] = rsd['max_rsd']['max_rsd']
+	combined_stations['MLT'] = rsd['max_rsd']['MLT']
 
 	segmented_df = combined_stations[combined_stations.index.isin(map_keys)]
 
@@ -134,7 +140,6 @@ def get_seconds(dt):
 	Returns:
 		ut (float): time in seconds from the 1970's
 	'''
-	print('Getting seconds....')
 	t0 = datetime(1970,1,1)
 	t1 = parser.parse(dt)
 	ut = (t1-t0).total_seconds()
@@ -164,7 +169,6 @@ def get_footpoint(xx=None, yy=None, zz=None, x_gsm=None, y_gsm=None, z_gsm=None,
 		yf (float): y component of the footpoint in GSM
 		zmin (float): z component of the footpoint in GSM (should be zero or close to zero)
 	'''
-	print('Getting footpoint....')
 	if x_gsm:
 		# Calculate dipole tilt angle
 		if dt:
@@ -207,7 +211,6 @@ def field_line_tracing(date, geolat, geolon, vx, vy, vz):
 		yf (float): y component of the footpoint in GSM
 		zmin (float): z component of the footpoint in GSM (should be zero or close to zero)
 	'''
-	print('Doing field line tracing....')
 	# getting time in seconds from date in the 1970's. Seems like a silly way to do this but that's the requirement.
 	ut = get_seconds(date)
 
@@ -217,19 +220,19 @@ def field_line_tracing(date, geolat, geolon, vx, vy, vz):
 	# convert degrees to radians
 	lat_rad = np.deg2rad(geolat)
 	lon_rad = np.deg2rad(geolon)
-	print(lat_rad, lon_rad)
+	# print(lat_rad, lon_rad)
 
 	# Convert Geodetic to geocentric spherical
 	r, theta_rad = geopack.geodgeo(0, lat_rad, 1)
-	print(r, theta_rad, lon_rad)
+	# print(r, theta_rad, lon_rad)
 
 	# Converting Geocentric Spherical to Geocentric Cartesian
 	x_gc, y_gc, z_gc = geopack.sphcar(1, theta_rad, lon_rad, 1)
-	print('GC:  ', x_gc,y_gc,z_gc,' R=',np.sqrt(x_gc**2+y_gc**2+z_gc**2))
+	# print('GC:  ', x_gc,y_gc,z_gc,' R=',np.sqrt(x_gc**2+y_gc**2+z_gc**2))
 
 	# Convert Geocentric Cartesian to GSM
 	x_gsm, y_gsm, z_gsm = geopack.geogsm(x_gc, y_gc, z_gc, 1)
-	print('GSM: ', x_gsm,y_gsm,z_gsm,' R=',np.sqrt(x_gsm**2+y_gsm**2+z_gsm**2))
+	# print('GSM: ', x_gsm,y_gsm,z_gsm,' R=',np.sqrt(x_gsm**2+y_gsm**2+z_gsm**2))
 
 	# perfroming the trace
 	x, y, z, xx, yy, zz = geopack.trace(x_gsm, y_gsm, z_gsm, dir=1, rlim=1000, r0=.99999, parmod=2, exname='t89', inname='igrf', maxloop=10000)
@@ -270,7 +273,7 @@ def preparing_region_footpoints_for_plotting(region_df, footpoints):
 														'zmin':foot['zmin'], 'station':station,
 														'dbdt':region_df[f'{station}_dbdt'],
 														'plotting_color':(region_df[station]-region_df['reg_mean'])},
-														axis=0, ignore_index=True)
+														ignore_index=True)
 
 	return scatter_plotting_df
 
@@ -314,7 +317,7 @@ def plotting_footpoints_on_twins_maps(twins_dict, region_df, date, region):
 	# adding footpoints colorbar
 	footpoint_cbar = plt.colorbar(footpoint_sc, ax=ax2, label='Mean Subtracted dB/dt')
 
-	plt.set_title(f'{date} - {region} Max RSD: {np.round(region_df[date]["rsd"], 2)} MLT: {np.round(region_df[date]["MLT"], 2)}', fontsize=25)
+	plt.title(f'{date} - {region} Max RSD: {np.round(region_df[date]["rsd"], 2)} MLT: {np.round(region_df[date]["MLT"], 2)}', fontsize=25)
 	plt.savefig(f'plots/footpoints_on_twins_maps/{region}_{date}.png')
 	plt.close()
 	gc.collect()
@@ -345,18 +348,19 @@ def main():
 	test_Stats = stats[region]
 
 	# getting dbdt and rsd data for the region
-	region_df = combining_regional_dfs(test_Region['stations'], test_Stats, twins.keys())
+	region_df = combining_regional_dfs(test_Region['station'], test_Stats, twins.keys())
 
 	# Getting the geographic coordiantes of the stations in the region
 	stations_geo_locations = {}
-	for station in test_Region['stations']:
+	for station in test_Region['station']:
 		df = loading_supermag(station)
 		stations_geo_locations[station] = {'GEOLAT': df['GEOLAT'].mean(), 'GEOLON': df['GEOLON'].mean()}
 
 	# getting the footpoints for each station in the region for each of
 	# the twins maps and storing them in the maps dictionary
+	print('Getting footpoints....')
 	for date, entry in twins.items():
-		if entry[f'{region}_footpoints']:
+		if f'{region}_footpoints' in entry:
 			continue
 		print(f'Working on {date}')
 		footpoints = {}
@@ -366,7 +370,7 @@ def main():
 														solarwind.loc[date]['Vy'], solarwind.loc[date]['Vz'])
 		entry[f'{region}_footpoints'] = footpoints
 
-	# saving the updated twins dictionary
+	# saving the updated twins dictionaryx
 	with open('../outputs/twins_maps_with_footpoints.pkl', 'wb') as f:
 		pickle.dump(twins, f)
 
