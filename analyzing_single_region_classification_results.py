d8382a85cfa7de6ead42a6b09707f4d4266abfbc
Mike Coughlan <mike.k.coughlan@gmail.com>
2023-10-28 15:39:06 -0400

    updating analysis file

--- analyzing_single_region_classification_results.py
+++ analyzing_single_region_classification_results.py
@@ -11,237 +11,233 @@ from sklearn.metrics import (auc, brier_score_loss, confusion_matrix,
 
 REGIONS = [194, 270, 287, 207, 62, 241, 366, 387, 223, 19, 163]
 
+data_dir = '../../../../data/'
+supermag_dir = data_dir+'supermag/feather_files/'
+regions_dict = data_dir+'mike_working_dir/identifying_regions_data/adjusted_regions.pkl'
+
 VERSION = 0
+TARGET = 'rsd'
 
 def load_predictions():
 
-	if os.path.exists(f'outputs/non_twins_modeling_region_{region}_version_{VERSION}.feather'):
-		predictions = pd.read_feather(f'outputs/non_twins_modeling_region_{region}_version_{VERSION}.feather')
+	if os.path.exists(f'outputs/{TARGET}/non_twins_modeling_region_{region}_version_{VERSION}.feather'):
+		predictions = pd.read_feather(f'outputs/{TARGET}/non_twins_modeling_region_{region}_version_{VERSION}.feather')
 		predictions.set_index('Date_UTC', inplace=True)
 	else:
-		raise(f'You fool! You need to run the script modeling_v{VERSION}.py first. Throw yourself down next time and rid us of your stupidity!')
-
+		raise(f'Fool of a Took! You need to run the script modeling_v{VERSION}.py first. Throw yourself down next time and rid us of your stupidity!')
 
 	return predictions
 
 
-def load_segmented_mlt_dict():
+def attaching_mlt(predictions, region):
 
-	with open(f'outputs/mlt_span_{MLT_SPAN}_dbdt.pkl', 'rb') as f:
-		segmented_mlt = pickle.load(f)
+	''' Function that attaches the MLT information to the predictions.'''
 
-	return segmented_mlt
+	# loading the segmented MLT data
+	with open(regions_dict, 'rb') as f:
+		regions = pickle.load(f)
+
+	stations = regions[region]['station']
+
+	longitudes = {}
+	latitudes = []
+	for station in stations:
+		temp_df = pd.read_feather(f'{supermag_dir}{station}.feather')
+		longitudes[station] = temp_df['GEOLON'].iloc[0]
+		latitudes.append(temp_df['MLAT'].iloc[0])
+
+	# getting the key of the maximum value of the longitude
+	max_longitude_station = max(longitudes, key=longitudes.get)
+
+	mlt = pd.read_feather(f'{supermag_dir}{max_longitude_station}.feather')
+	mlt.set_index('Date_UTC', inplace=True)
+	mlt = mlt['MLT']
+
+	# getting the MLT information for each prediction
+	predictions = predictions.join(mlt, how='left')
+
+	return predictions, latitudes.mean()
 
 
-def plotting_simple_scatter(predictions):
+def plotting_simple_scatter(all_predictions):
 
 	''' Function that plots a simple scatter plot of the predictions vs the actual values'''
 
-	fig = plt.figure(figsize=(20,10))
-	ax = fig.add_subplot(111)
-	ax.scatter(predictions['actual'], predictions['predicted'], s=10)
-	ax.set_xlabel('Actual Values')
-	ax.set_ylabel('Predictions')
-	ax.set_title('Predictions vs Actual Values')
-	ax.plot([0, 1], [0, 1], transform=ax.transAxes, ls='--', c='r')
+	for region all_predictions.keys():
+		predictions = all_predictions[region]['dataframe']
+		fig = plt.figure(figsize=(20,10))
+		ax = fig.add_subplot(111)
+		ax.scatter(predictions['actual'], predictions['predicted'], s=10)
+		ax.set_xlabel('Actual Values')
+		ax.set_ylabel('Predictions')
+		ax.set_title(f'Predictions vs Actual Values {region}')
+		ax.plot([0, 1], [0, 1], transform=ax.transAxes, ls='--', c='r')
 
-	plt.savefig(f'plots/analysis_plots_modeling_v{VERSION}/simple_scatter_plot.png')
+		plt.savefig(f'plots/{TARGET}/analysis_plots_modeling_v{VERSION}/simple_scatter_plot.png')
 
 
-def simple_scatter_with_latitude_information(predictions, segmented_mlt):
-
-	''' Function that plots a simple scatter plot of the predictions vs the actual values
-			with the latitude information for each prediction.'''
-
-	# getting the latitude information for each prediction
-	predictions['latitude'] = predictions.index.map(lambda x: segmented_mlt[f'{MLT_BIN_TARGET}'].loc[x.strftime(format='%Y-%m-%d %H:%M:%S')]['mean_lat'])
+def prediction_error_vs_MLT(all_predictions):
 
 	fig = plt.figure(figsize=(20,10))
 	ax = fig.add_subplot(111)
-	scatter = ax.scatter(predictions['actual'], predictions['predicted'], s=10, c=predictions['latitude'], cmap='plasma')
-	plt.colorbar(scatter)
+	for region in all_predictions.keys():
+		predictions = all_predictions[region]['dataframe']
+		error = predictions['actual'] - predictions['predicted']
+		plt.scatter(predictions['MLT'], error, s=10, label=region)
+	plt.legend()
 	ax.set_xlabel('Actual Values')
 	ax.set_ylabel('Predictions')
-	ax.set_title('Predictions vs Actual Values')
+	ax.set_title('actual-predicted vs MLT')
 	ax.plot([0, 1], [0, 1], transform=ax.transAxes, ls='--', c='r')
 
-	plt.savefig(f'plots/analysis_plots_modeling_v{VERSION}/simple_scatter_plot_with_latitude.png')
+	plt.savefig(f'plots/{TARGET}/analysis_plots_modeling_v{VERSION}/simple_scatter_plot_with_latitude.png')
 
 
-def plotting_reliability_diagram(predictions):
+def plotting_reliability_diagram(all_predictions):
 
 	''' Function that plots the reliability diagram for the predictions.'''
 
-	# getting the reliability diagram
-	fraction_of_positives, mean_predicted_value = calibration_curve(predictions['actual'], predictions['predicted'], n_bins=10)
-
 	fig = plt.figure(figsize=(20,10))
 	ax = fig.add_subplot(111)
-	ax.plot(mean_predicted_value, fraction_of_positives, 's-', label='MLT Bin')
+	for region in all_predictions.keys():
+		# getting the reliability diagram
+		predictions = all_predictions[region]['dataframe']
+		fraction_of_positives, mean_predicted_value = calibration_curve(predictions['actual'], predictions['predicted'], n_bins=10)
+		plt.plot(mean_predicted_value, fraction_of_positives, 's-', label=all_predictions[region]['average_mlat'])
 	ax.plot([0, 1], [0, 1], '--', color='gray', label='Perfectly Calibrated')
 	ax.set_xlabel('Mean Predicted Value')
 	ax.set_ylabel('Fraction of Positives')
 	ax.set_title('Reliability Diagram')
 	ax.legend()
 
-	plt.savefig(f'plots/analysis_plots_modeling_v{VERSION}/reliability_diagram.png')
+	plt.savefig(f'plots/{TARGET}/analysis_plots_modeling_v{VERSION}/reliability_diagram.png')
 
 
-def plotting_precision_recall_curve(predictions):
+def plotting_precision_recall_curve(all_predictions):
 
 	''' Function that plots the precision recall curve for the predictions.'''
 
-	# getting the precision recall curve
-	precision, recall, thresholds = precision_recall_curve(predictions['actual'], predictions['predicted'])
-
-	# getting the area under the curve
-	auc_score = auc(recall, precision)
-
 	fig = plt.figure(figsize=(20,10))
 	ax = fig.add_subplot(111)
-	ax.plot(recall, precision, label='MLT Bin')
+	for region in all_predictions.keys():
+		# getting the precision recall curve
+		precision, recall, thresholds = precision_recall_curve(predictions['actual'], predictions['predicted'])
+		# getting the area under the curve
+		auc_score = auc(recall, precision)
+
+		plt..plot(recall, precision, label=f'{all_predictions[region]['average_mlat']}; AUC: {auc_score:.3f}')
 	ax.set_xlabel('Recall')
 	ax.set_ylabel('Precision')
 	ax.set_title(f'Precision Recall Curve; AUC: {auc_score:.3f}')
 	ax.legend()
 
-	plt.savefig(f'plots/analysis_plots_modeling_v{VERSION}/precision_recall_curve.png')
+	plt.savefig(f'plots/{TARGET}/analysis_plots_modeling_v{VERSION}/precision_recall_curve.png')
 
 
-def plotting_error_vs_latitude(predictions, segmented_mlt):
-
-	''' Function that plots the error vs the latitude for each prediction.'''
-
-	# getting the latitude information for each prediction
-	predictions['latitude'] = predictions.index.map(lambda x: segmented_mlt[f'{MLT_BIN_TARGET}'].loc[x.strftime(format='%Y-%m-%d %H:%M:%S')]['mean_lat'])
-
-	# getting the error for each prediction
-	predictions['error'] = predictions['predicted'] - predictions['actual']
-
-	fig = plt.figure(figsize=(20,10))
-	ax = fig.add_subplot(111)
-	ax.scatter(predictions['latitude'], predictions['error'], s=10)
-	ax.set_xlabel('Latitude')
-	ax.set_ylabel('Error')
-	ax.set_title('Error vs Latitude')
-
-	plt.savefig(f'plots/analysis_plots_modeling_v{VERSION}/error_vs_latitude.png')
-
-def plotting_actual_values_vs_latitude(predictions, segmented_mlt):
-
-	''' Function that plots the actual values vs the latitude for each prediction.'''
-
-	# getting the latitude information for each prediction
-	predictions['latitude'] = predictions.index.map(lambda x: segmented_mlt[f'{MLT_BIN_TARGET}'].loc[x.strftime(format='%Y-%m-%d %H:%M:%S')]['mean_lat'])
-
-	fig = plt.figure(figsize=(20,10))
-	ax = fig.add_subplot(111)
-	ax.scatter(predictions['latitude'], predictions['actual'], s=10)
-	ax.set_xlabel('Latitude')
-	ax.set_ylabel('Actual Values')
-	ax.set_title('Actual Values vs Latitude')
-
-	plt.savefig(f'plots/analysis_plots_modeling_v{VERSION}/actual_values_vs_latitude.png')
-
-
-def plotting_predicted_values_vs_latitude(predictions, segmented_mlt):
+def plotting_predicted_values_vs_latitude(all_predictions):
 
 	''' Function that plots the predicted values vs the latitude for each prediction.'''
 
-	# getting the latitude information for each prediction
-	predictions['latitude'] = predictions.index.map(lambda x: segmented_mlt[f'{MLT_BIN_TARGET}'].loc[x.strftime(format='%Y-%m-%d %H:%M:%S')]['mean_lat'])
-
 	fig = plt.figure(figsize=(20,10))
 	ax = fig.add_subplot(111)
-	ax.scatter(predictions['latitude'], predictions['predicted'], s=10)
+	for region in all_predictions.keys():
+		predictions = all_predictions[region]['dataframe']
+		plt.scatter([all_predictions[region]['average_mlat'] for i in range(len(predictions))], predictions['predicted'], s=10)
 	ax.set_xlabel('Latitude')
 	ax.set_ylabel('Predicted Values')
 	ax.set_title('Predicted Values vs Latitude')
 
-	plt.savefig(f'plots/analysis_plots_modeling_v{VERSION}/predicted_values_vs_latitude.png')
+	plt.savefig(f'plots/{TARGET}/analysis_plots_modeling_v{VERSION}/predicted_values_vs_latitude.png')
 
 
-def calculating_scores(predictions):
+def calculating_scores(all_predictions):
 
 	'''
 	Function that calculates the scores for the predictions.
 	'''
+	all_scores = pd.DataFrame()
+	for region in all_predictions.keys():
+		predictions = all_predictions[region]['dataframe']
 
-	# manually calculating the elements of the confusion matrix using 0.5 as the probabalistic prediction threshold
-	Tp = len(predictions[(predictions['actual'] == 1) & (predictions['predicted'] >= 0.5)])
-	Tn = len(predictions[(predictions['actual'] == 0) & (predictions['predicted'] < 0.5)])
-	Fp = len(predictions[(predictions['actual'] == 0) & (predictions['predicted'] >= 0.5)])
-	Fn = len(predictions[(predictions['actual'] == 1) & (predictions['predicted'] < 0.5)])
+		# manually calculating the elements of the confusion matrix using 0.5 as the probabalistic prediction threshold
+		Tp = len(predictions[(predictions['actual'] == 1) & (predictions['predicted'] >= 0.5)])
+		Tn = len(predictions[(predictions['actual'] == 0) & (predictions['predicted'] < 0.5)])
+		Fp = len(predictions[(predictions['actual'] == 0) & (predictions['predicted'] >= 0.5)])
+		Fn = len(predictions[(predictions['actual'] == 1) & (predictions['predicted'] < 0.5)])
 
-	# calculating brier score
-	Bs = (brier_score_loss(predictions['actual'], predictions['predicted']))
+		# calculating brier score
+		Bs = (brier_score_loss(predictions['actual'], predictions['predicted']))
 
-	# calculating the heidke skill score
-	Hss = 2*((Tp*Tn)-(Fp*Fn))/(((Tp+Fn)*(Tn+Fn))+((Tp+Fp)*(Tn+Fp)))
+		# calculating the heidke skill score
+		Hss = 2*((Tp*Tn)-(Fp*Fn))/(((Tp+Fn)*(Tn+Fn))+((Tp+Fp)*(Tn+Fp)))
 
-	# calculating the precision and recall
-	try:
-		precision = Tp/(Tp+Fp)
-	except ZeroDivisionError:
-		precision = 0
+		# calculating the precision and recall
+		try:
+			precision = Tp/(Tp+Fp)
+		except ZeroDivisionError:
+			precision = 0
 
-	try:
-		recall = Tp/(Tp+Fn)
-	except ZeroDivisionError:
-		recall = 0
+		try:
+			recall = Tp/(Tp+Fn)
+		except ZeroDivisionError:
+			recall = 0
 
-	# calculating the f1 score
-	try:
-		f1 = 2*((precision*recall)/(precision+recall))
-	except ZeroDivisionError:
-		f1 = 0
+		# calculating the f1 score
+		try:
+			f1 = 2*((precision*recall)/(precision+recall))
+		except ZeroDivisionError:
+			f1 = 0
 
-	# calculating the rmse
-	rmse = np.sqrt(mean_squared_error(predictions['actual'], predictions['predicted']))
+		# calculating the rmse
+		rmse = np.sqrt(mean_squared_error(predictions['actual'], predictions['predicted']))
 
-	# putting it all into a dataframe
-	scores = pd.DataFrame({'True_pos':Tp, 'False_pos':Fp, 'False_neg': Fn, 'True_neg':Tn,
-								'Brier Score':Bs, 'Hss':Hss, 'precision':precision,
-								'recall':recall, 'f1':f1, 'rmse':rmse}, index=[0])
+		# putting it all into a dataframe
+		scores = pd.DataFrame({'True_pos':Tp, 'False_pos':Fp, 'False_neg': Fn, 'True_neg':Tn,
+									'Brier Score':Bs, 'Hss':Hss, 'precision':precision,
+									'recall':recall, 'f1':f1, 'rmse':rmse}, index=[region])
 
-	return scores
+		all_scores = pd.concat([all_scores, scores.T], axis=1)
+
+	return all_scores
 
 
 def main():
 
-	if not os.path.exists(f'plots/analysis_plots_modeling_v{VERSION}/'):
-		os.makedirs(f'plots/analysis_plots_modeling_v{VERSION}/')
+	if not os.path.exists(f'plots/{TARGET}/analysis_plots_modeling_v{VERSION}/'):
+		os.makedirs(f'plots/{TARGET}/analysis_plots_modeling_v{VERSION}/')
 
-	if not os.path.exists(f'outputs/analysis_plots_modeling_v{VERSION}/'):
-		os.makedirs(f'outputs/analysis_plots_modeling_v{VERSION}/')
+	if not os.path.exists(f'outputs/{TARGET}/analysis_plots_modeling_v{VERSION}/'):
+		os.makedirs(f'outputs/{TARGET}/analysis_plots_modeling_v{VERSION}/')
 
-	predictions = load_predictions(use_dict=False)
-	segmented_mlt = load_segmented_mlt_dict()
+	all_predictions = {region:{} for region in REGIONS}
+
+	for region in REGIONS:
+		predictions = load_predictions()
+		predictions, average_mlat = attaching_mlt(predictions)
+		all_predictions[region]['dataframe'] = predictions
+		all_predictions[region]['average_mlat'] = average_mlat
 
 	# plotting a simple scatter plot of the predictions vs the actual values
-	plotting_simple_scatter(predictions)
+	plotting_simple_scatter(all_predictions)
 
 	# plotting a simple scatter plot of the predictions vs the actual values with the latitude information for each prediction
-	simple_scatter_with_latitude_information(predictions, segmented_mlt)
+	prediction_error_vs_MLT(all_predictions, average_mlat)
 
 	# plotting the reliability diagram for the predictions
-	plotting_reliability_diagram(predictions)
+	plotting_reliability_diagram(all_predictions)
 
 	# plotting the precision recall curve for the predictions
-	plotting_precision_recall_curve(predictions)
-
-	# plotting the error vs the latitude for each prediction
-	plotting_error_vs_latitude(predictions, segmented_mlt)
+	plotting_precision_recall_curve(all_predictions)
 
 	# plotting the actual values vs the latitude for each prediction
-	plotting_actual_values_vs_latitude(predictions, segmented_mlt)
+	plotting_actual_values_vs_latitude(all_predictions)
 
 	# plotting the predicted values vs the latitude for each prediction
-	plotting_predicted_values_vs_latitude(predictions, segmented_mlt)
+	plotting_predicted_values_vs_latitude(all_predictions)
 
 	# calculating the scores for the predictions
-	scores = calculating_scores(predictions)
-	scores.to_csv(f'outputs/analysis_plots_modeling_v{VERSION}/scores.csv', index=False)
+	scores = calculating_scores(all_predictions)
+	scores.to_csv(f'outputs/{TARGET}/analysis_plots_modeling_v{VERSION}/scores.csv')
 
 	print('Done!')
 
